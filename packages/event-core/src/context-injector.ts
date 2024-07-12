59b7f8fb53edc562582d97065ca47b96feed0641
mav-rik <mav777art@gmail.com>
2024-07-12 16:01:46 +0200

    chore: improve types

--- packages/event-core/src/context-injector.ts
+++ packages/event-core/src/context-injector.ts
@@ -4,15 +4,15 @@
  * Provides a way to inject context
  * Usefull when working with opentelemetry spans
  */
-export class ContextInjector {
+export class ContextInjector<N> {
   with<T>(
-    name: string,
+    name: N,
     attributes: Record<string, string | number | boolean>,
     cb: TContextInjectorCallback<T>
   ): T
-  with<T>(name: string, cb: TContextInjectorCallback<T>): T
+  with<T>(name: N, cb: TContextInjectorCallback<T>): T
   with<T>(
-    name: string,
+    name: N,
     attributes: Record<string, string | number | boolean> | TContextInjectorCallback<T>,
     cb?: TContextInjectorCallback<T>
   ): T {
@@ -25,10 +25,10 @@ type TContextInjectorCallback<T> = () => T
 
 let ci = new ContextInjector()
 
-export function getContextInjector() {
-  return ci
+export function getContextInjector<N = 'Event:start'>() {
+  return ci as ContextInjector<N>
 }
 
-export function replaceContextInjector(newCi: ContextInjector) {
+export function replaceContextInjector(newCi: ContextInjector<string>) {
   ci = newCi
 }
